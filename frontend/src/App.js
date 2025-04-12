d4efe2936446ff02a58ece79048ca950c727dc84
Luke Wagner <lukedwagner01@gmail.com>
2025-04-12 16:43:47 -0700

    Rename AdminPage to AdminLogin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import LandingPage from './components/LandingPage';
-import AdminLogin from './components/AdminPage';
+import AdminLogin from './components/AdminLogin';
 import AdminDashboard from './components/AdminDashboard';
 
 // Import CSS files
